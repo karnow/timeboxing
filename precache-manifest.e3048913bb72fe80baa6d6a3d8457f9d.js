211bf761608bd3a25d5f411d8b7704aae32fd6cd
carl <karol-nowakowski@o2.pl>
2020-12-10 09:08:28 +0100

    Updates

---
+++ precache-manifest.e3048913bb72fe80baa6d6a3d8457f9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46fe4669e76e6bbb4c1768e4ffcbd062",
+    "url": "/timeboxing/index.html"
+  },
+  {
+    "revision": "ab8eff31f36704f95bef",
+    "url": "/timeboxing/static/css/main.ed90723b.chunk.css"
+  },
+  {
+    "revision": "5e0ee6885f0eba36e935",
+    "url": "/timeboxing/static/js/2.3eb87d2d.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/timeboxing/static/js/2.3eb87d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdad611d3450aa7d4e31",
+    "url": "/timeboxing/static/js/3.2f648a86.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/timeboxing/static/js/3.2f648a86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60413186ddd1aadbf8f3",
+    "url": "/timeboxing/static/js/4.7778b7e9.chunk.js"
+  },
+  {
+    "revision": "ab8eff31f36704f95bef",
+    "url": "/timeboxing/static/js/main.ec1dc7a4.chunk.js"
+  },
+  {
+    "revision": "9ab3ecefcfc4c8255097",
+    "url": "/timeboxing/static/js/runtime-main.77ca0b78.js"
+  }
+]);
