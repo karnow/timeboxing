85ddba29cb7ba76833080990cf7da76afd23d99a
Karol Nowakowski <karolnowakowski@MacBook-Pro-Karol.local>
2021-11-17 22:48:55 +0100

    Updates

---
+++ precache-manifest.8c73c5fdac767a6d17fac3ddaebecf17.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89fb375f882bc6a786074ca0a11ae52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "575c18859a91e886aeb8",
+    "url": "/static/css/main.65da3415.chunk.css"
+  },
+  {
+    "revision": "4feec4138c6a5a09a3be",
+    "url": "/static/js/2.68b9322c.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/static/js/2.68b9322c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f5458c96792639a4061",
+    "url": "/static/js/3.fb1d6d7e.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/3.fb1d6d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8aebacbf631022e449bd",
+    "url": "/static/js/4.cb754aa4.chunk.js"
+  },
+  {
+    "revision": "575c18859a91e886aeb8",
+    "url": "/static/js/main.2f4f4229.chunk.js"
+  },
+  {
+    "revision": "c03b420cd3519e71c676",
+    "url": "/static/js/runtime-main.431f4230.js"
+  }
+]);
